fe6cf8a32c52f107da50e38428d6046223eff46f
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-11-26 17:53:48 +0200

    Fix format

--- src/pages/CheckoutPage.js
+++ src/pages/CheckoutPage.js
@@ -9,7 +9,7 @@ class CheckoutPage extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state        = {
+    this.state = {
       submitted: false,
       order: {},
       form: {}
