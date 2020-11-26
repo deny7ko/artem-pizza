28587aea9e68af6b3235d5da009d01de4f9e7dad
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-11-26 18:02:41 +0200

    Migrate Layout

--- src/Layout.js
+++ src/Layout.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-class Layout extends React.Component {
-  render() {
-    return (
-      <div style={{ padding: "2em" }}>
-        <nav>
-          <p>
-            <Link to="/register">Register</Link> or <Link to="/login">Login</Link>
-          </p>
+const Layout = ({ children }) => {
+  return (
+    <div style={{ padding: "2em" }}>
+      <nav>
+        <p>
+          <Link to="/register">Register</Link> or <Link to="/login">Login</Link>
+        </p>
 
-          <p>
-            <Link to="/constructor">Select Pizza</Link>
-          </p>
-        </nav>
-        {this.props.children}
-      </div>
+        <p>
+          <Link to="/constructor">Select Pizza</Link>
+        </p>
+      </nav>
+      {children}
+    </div>
 
-    );
-  }
+  );
 }
 
 export default Layout
