8e55cf8e263146121d52fe64b97fbb7a59c16af3
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-11-27 20:04:13 +0200

    Add product form

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,18 +1,40 @@
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Layout from './Layout'
-import ProductCreationPage from './ProductCreationPage'
+import { useForm } from "react-hook-form";
 
-function App() {const App = () => {
+const App = () => {
+  const { register, handleSubmit } = useForm();
+
+  const submitForm = (data) => {
+    console.log(data)
+  }
+
   return (
   <Router>
-    <Layout>
-      <Route exact path="/admin">
-        <h1>Hello Admin</h1>
-      </Route>
-    </Layout>
+    <Route exact path="/admin">
+      <form onSubmit={handleSubmit(submitForm)}>
+        <div>
+          <label >Price</label>
+          <input ref={register} type="tel" name="price"></input>
+        </div>
+        <div>
+          <label>Name</label>
+          <input ref={register} type="text" name="name"></input>
+        </div>
+        <div>
+          <label>Slug</label>
+          <input ref={register} type="text" name="slug"></input>
+        </div>
+        <div>
+          <label>Picture</label>
+          <input ref={register} type="file" name="picture"></input>
+        </div>
+
+        <button>Create product</button>
+      </form>
+    </Route>
   </Router>
   )
 }
 
-export default App;
+export default App
