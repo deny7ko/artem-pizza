9e26cdc6882de73e5dba5b3fc6c98c68f1dddbe9
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-11-27 19:55:11 +0200

    Add admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Layout from './Layout'
+import ProductCreationPage from './ProductCreationPage'
 
-function App() {
+function App() {const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Router>
+    <Layout>
+      <Route exact path="/admin">
+        <h1>Hello Admin</h1>
+      </Route>
+    </Layout>
+  </Router>
+  )
 }
 
 export default App;
