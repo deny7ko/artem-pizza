7c3bf345b143b1185b6c92035419fe0c37572554
Denys Medynskyi <denys.medynskyi@toptal.com>
2020-12-09 05:48:43 +0200

    Add delete link

--- admin/src/components/Ingredient.js
+++ admin/src/components/Ingredient.js
@@ -1,11 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { deleteIngredient } from 'api'
+import { useMutation, useQueryCache } from 'react-query'
 
 const Ingredient = ({id, name, price}) => {
+  const queryCache = useQueryCache()
+  const [deleteMutation] = useMutation(deleteIngredient)
+
+  const handleDeleteClick = async (e) => {
+    await deleteMutation(id)
+    queryCache.invalidateQueries('ingredientList')
+  }
+
   return (
     <>
       <p><b>name:</b>{name}</p>
       <p><b>price:</b>{price}</p>
+
+      <p style={{color: 'red'}}>
+        <a href="#" onClick={handleDeleteClick}>
+          Delete
+        </a>
+      </p>
+
+      <p style={{color: 'blue'}}>
+        <Link to={`/admin/ingredients/${id}`}>Edit</Link>
+      </p>
     </>
   )
 }
--- admin/src/pages/IngredientListPage/IngredientListPage.js
+++ admin/src/pages/IngredientListPage/IngredientListPage.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { useQuery } from 'react-query'
-import { getIngredientList, deleteIngredient } from 'api'
+import { getIngredientList } from 'api'
 import Ingredient from 'components/Ingredient'
-import { Link } from "react-router-dom";
 
 const IngredientListPage = () => {
-  const { isLoading, isError, data: ingredientList, error } = useQuery('ingredientList', getIngredientList)
+  const { isLoading, data: ingredientList } = useQuery('ingredientList', getIngredientList)
 
   if (isLoading) {
     return <>Loading ...</>
@@ -21,16 +20,6 @@ const IngredientListPage = () => {
         return (
           <div key={ingredient.id}>
             <Ingredient {...ingredient} />
-
-            <p style={{color: 'red'}}>
-              <a href="#" onClick={handleClick}>
-                Delete
-              </a>
-            </p>
-
-            <p style={{color: 'blue'}}>
-              <Link to={`/admin/ingredients/${ingredient.id}`}>Edit</Link>
-            </p>
             <hr />
           </div>
         )
