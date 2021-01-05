fb4d049bb9d97c90177190acf41546f734bc9867
Denys Medynskyi <denys.medynskyi@toptal.com>
2021-01-05 19:08:19 +0200

    Add fullname function

--- src/index.js
+++ src/index.js
@@ -35,6 +35,8 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
+const fullName = () => {};
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
